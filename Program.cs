518e008a9221d4062a4504b3cdf545047214e114
williamDresch016 <135863277+williamDresch016@users.noreply.github.com>
2023-06-12 17:05:48 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula07jogos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Random sorteador = new Random();
+            int numero;
+            int sorteio = sorteador.Next(1,11);
+            
+            Console.WriteLine("Bem-vindo a conradito jogos \n");
+
+            Console.WriteLine("Advinhe o numero entre 1 e 10");
+            Console.WriteLine("Digite o numero:");
+            numero = int.Parse(Console.ReadLine());
+
+            if (numero < 1 || numero > 10 )
+            {
+                Console.WriteLine("Digite um numero valido");
+                Main(null);
+                return;
+            }
+
+            if (numero == sorteio )
+            {
+                Console.WriteLine("Parabens,voce venceu o jogo");
+
+            Console.ReadKey();
+                return;
+            }
+            Console.WriteLine("Voce errou ! tente novamente");
+                Main(null);
+            Console.ReadKey();
+
+
+        }
+    }
+}
